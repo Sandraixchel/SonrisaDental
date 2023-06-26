ac8d215a51883e276bee4894707a131a5605f527
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-06-26 22:19:46 +0100

    Initial Commit

---
+++ src/main/java/com/sandraixchel/SonrisaDental/SonrisaDentalApplication.java
@@ -0,0 +1,13 @@
+package com.sandraixchel.SonrisaDental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonrisaDentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonrisaDentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandraixchel/SonrisaDental/SonrisaDentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandraixchel.SonrisaDental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonrisaDentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
