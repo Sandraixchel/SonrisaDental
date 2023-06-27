6ed1398d74f102ea80995936ba12841177b41383
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-06-28 00:55:56 +0100

    Serving Web Content-Set Up

---
+++ src/main/java/com/sandraixchel/SonrisaDental/controller/AppointmentController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sandraixchel.SonrisaDental.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Sandy
+ */
+
+@Controller
+public class AppointmentController {
+    
+    @GetMapping("/greeting")
+	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+    
+}
