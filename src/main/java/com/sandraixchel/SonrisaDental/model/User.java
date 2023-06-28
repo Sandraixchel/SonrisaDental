3d97cb99dd17a8a7fe90b02b69add026e9d01c48
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-06-28 12:57:16 +0100

    Connecting to MySQL

---
+++ src/main/java/com/sandraixchel/SonrisaDental/model/User.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sandraixchel.SonrisaDental.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+/**
+ *
+ * @author Sandy
+ */
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer id;
+
+  private String name;
+
+  private String email;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
---
+++ src/main/java/com/sandraixchel/SonrisaDental/model/UserRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sandraixchel.SonrisaDental.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+  
+
+}
+
