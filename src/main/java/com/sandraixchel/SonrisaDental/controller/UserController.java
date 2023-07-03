d9b69c29dd6f9ef4eb2f6699ae81a71905965ec3
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-07-03 22:19:11 +0100

    PUT and DELETE mapping added

--- src/main/java/com/sandraixchel/SonrisaDental/controller/UserController.java
+++ src/main/java/com/sandraixchel/SonrisaDental/controller/UserController.java
@@ -10,9 +10,11 @@ import com.sandraixchel.SonrisaDental.model.UserRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,4 +45,33 @@ public class UserController {
     return userRepository.findById(id)
             .orElseThrow(()->new UserNotFoundException(id));
     }
+    //For editing the mapping
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Integer id){
+    return userRepository.findById(id)
+            .map(user -> {
+                user.setUsername(newUser.getUsername());
+                user.setName(newUser.getName());
+                user.setEmail(newUser.getEmail());
+                return userRepository.save(user);
+                
+            }).orElseThrow(()->new UserNotFoundException(id));
+            
+    }    
+    
+    //To delete data from the data base
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Integer id ){
+    if(!userRepository.existsById(id)){
+        throw new UserNotFoundException(id);
+    }
+    
+    userRepository.deleteById(id);
+    return "User with ID " + id + " has been deleted successfully";
+    
+    }
+    
+    
+    
+    
 }
