ed1b5e42f7a7cde9fec00cb0731c5c81e8e45455
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-07-06 17:12:03 +0100

    Appointment Controller (POST,GET,DELETE,PUT) working

--- src/main/java/com/sandraixchel/SonrisaDental/controller/AppointmentController.java
+++ src/main/java/com/sandraixchel/SonrisaDental/controller/AppointmentController.java
@@ -4,18 +4,82 @@
  */
 package com.sandraixchel.SonrisaDental.controller;
 
-import org.springframework.stereotype.Controller;
+import com.sandraixchel.SonrisaDental.exception.AppointmentNotFoundException;
+import com.sandraixchel.SonrisaDental.model.Appointment;
+import com.sandraixchel.SonrisaDental.repository.AppointmentRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  * @author Sandy
  */
 
-@Controller
+@RestController //It eliminates the need for annotating each method with @ResponseBody.
 public class AppointmentController {
+    
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+    
+    //Request to add a new appointment
+    @PostMapping("/appointment") 
+    Appointment newAppointment(@RequestBody Appointment newAppointment){
+        return appointmentRepository.save(newAppointment);
+    }
+    
+    //Request to view all appointments 
+    @GetMapping("/appointments")
+    List<Appointment> getAllAppointments(){
+        return (List<Appointment>) appointmentRepository.findAll();
+    }
+    
+    //Request to view appointment by ID
+    @GetMapping("/appointment/{id}")
+    Appointment getAppointmentById(@PathVariable Integer id){
+    return appointmentRepository.findById(id)
+            .orElseThrow(()->new AppointmentNotFoundException(id));
+    }
+    
+    //Request to edit the appointment
+    @PutMapping("/appointment/{id}")
+    Appointment updateAppoinment(@RequestBody Appointment newAppointment, @PathVariable Integer id){
+    return appointmentRepository.findById(id)
+            .map(appointment -> {
+                appointment.setType(newAppointment.getType());
+                appointment.setDate(newAppointment.getDate());
+                appointment.setStart_time(newAppointment.getStart_time());
+                appointment.setEnd_time(newAppointment.getEnd_time());
+                return appointmentRepository.save(appointment);
+                
+            }).orElseThrow(()->new AppointmentNotFoundException(id));
+            
+    }
+    
+    //Request to delete an appointment by id
+    @DeleteMapping("/appointment/{id}")
+     String deleteAppointment(@PathVariable Integer id ){
+    if(!appointmentRepository.existsById(id)){
+        throw new AppointmentNotFoundException(id);
+    }
+    
+    appointmentRepository.deleteById(id);
+    return "Appointment with ID " + id + " has been deleted successfully";
+    
+    }
+   
+    
+    
+    
+    
     
     @GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
