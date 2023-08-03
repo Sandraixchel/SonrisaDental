67ebdc983640e00a72a982c32bd6a103a264a4de
Sandra Ixchel López Acosta <115147887+Sandraixchel@users.noreply.github.com>
2023-08-03 23:43:52 +0100

    Sign In Form (POST) working

--- src/main/java/com/sandraixchel/SonrisaDental/controller/PatientController.java
+++ src/main/java/com/sandraixchel/SonrisaDental/controller/PatientController.java
@@ -4,8 +4,10 @@
  */
 package com.sandraixchel.SonrisaDental.controller;
 
+import com.sandraixchel.SonrisaDental.exception.IncorrectPasswordException;
 import com.sandraixchel.SonrisaDental.exception.PatientNotFoundException;
 import com.sandraixchel.SonrisaDental.model.Patient;
+import com.sandraixchel.SonrisaDental.objects.Login;
 import com.sandraixchel.SonrisaDental.repository.PatientRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,63 +24,79 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Sandy
  */
-
 @RestController //It eliminates the need for annotating each method with @ResponseBody.
 @CrossOrigin("http://localhost:3000") //To connect it with REACT
 public class PatientController {
-    
+
     @Autowired
     private PatientRepository patientRepository;
-    
+
     //Request to add a new patient
-    @PostMapping("/patient") 
-    Patient newPatient(@RequestBody Patient newPatient){
+    @PostMapping("/patient")
+    Patient newPatient(@RequestBody Patient newPatient) {
         return patientRepository.save(newPatient);
     }
-    
+
     //Request to view all patients 
     @GetMapping("/patients")
-    List<Patient> getAllPatients(){
+    List<Patient> getAllPatients() {
         return (List<Patient>) patientRepository.findAll();
     }
-    
+
     //Request to view patient by ID
     @GetMapping("/patient/{id}")
-    Patient getPatientById(@PathVariable Integer id){
-    return patientRepository.findById(id)
-            .orElseThrow(()->new PatientNotFoundException(id));
+    Patient getPatientById(@PathVariable Integer id) {
+        return patientRepository.findById(id)
+                .orElseThrow(() -> new PatientNotFoundException(id));
     }
-    
+
+//    //Request to view patient by email
+//    @GetMapping("/patient/{email}")
+//    Patient getPatientByEmail(@PathVariable String email){
+//    return patientRepository.findByEmail(email)
+//            .orElseThrow(()->new PatientNotFoundException(email));
+//    }
+    @PostMapping("/patientsignin")
+    Patient signInPatient(@RequestBody Login loginForm) { //Accessing the POST request body sent by the front end login form
+        Patient foundPatient = patientRepository.findFirstByEmail(loginForm.getEmail()); //Variable to store the patient which has the email address that the front end sent
+        if (foundPatient.getPassword().equals(loginForm.getPassword())) { //Once the patient is found if the founf patient password is equal to login form passwrod then it can log in, otherwise it will trhoe a incorrect password message
+            return foundPatient;
+
+        } else {
+
+            throw new IncorrectPasswordException();
+        }
+
+    }
+
     //Request to edit the patient details
     @PutMapping("/patient/{id}")
-    Patient updatePatient(@RequestBody Patient newPatient, @PathVariable Integer id){
-    return patientRepository.findById(id)
-            .map(patient -> {
-                patient.setFirst_name(newPatient.getFirst_name());
-                patient.setLast_name(newPatient.getLast_name());
-                patient.setDob(newPatient.getDob());
-                patient.setPhone_number(newPatient.getPhone_number());
-                patient.setEmail(newPatient.getEmail());
-                patient.setInsurance_provider(newPatient.getInsurance_provider());
-                patient.setInsurance_number(newPatient.getInsurance_number());
-                return patientRepository.save(patient);
-                
-            }).orElseThrow(()->new PatientNotFoundException(id));
-            
+    Patient updatePatient(@RequestBody Patient newPatient, @PathVariable Integer id) {
+        return patientRepository.findById(id)
+                .map(patient -> {
+                    patient.setFirst_name(newPatient.getFirst_name());
+                    patient.setLast_name(newPatient.getLast_name());
+                    patient.setDob(newPatient.getDob());
+                    patient.setPhone_number(newPatient.getPhone_number());
+                    patient.setEmail(newPatient.getEmail());
+                    patient.setInsurance_provider(newPatient.getInsurance_provider());
+                    patient.setInsurance_number(newPatient.getInsurance_number());
+                    return patientRepository.save(patient);
+
+                }).orElseThrow(() -> new PatientNotFoundException(id));
+
     }
-    
+
     //Request to delete a patient by id
     @DeleteMapping("/patient/{id}")
-     String deletePatient(@PathVariable Integer id ){
-    if(!patientRepository.existsById(id)){
-        throw new PatientNotFoundException(id);
+    String deletePatient(@PathVariable Integer id) {
+        if (!patientRepository.existsById(id)) {
+            throw new PatientNotFoundException(id);
+        }
+
+        patientRepository.deleteById(id);
+        return "Patient with ID " + id + " has been deleted successfully";
+
     }
-    
-    patientRepository.deleteById(id);
-    return "Patient with ID " + id + " has been deleted successfully";
-    
-    }
-    
-    
-    
+
 }
